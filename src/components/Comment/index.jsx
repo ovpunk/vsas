6036d8e67e8a5649d99f5ba746b32e59723f078c
Vyacheslav Ovchinnikov <ovpunx@gmail.com>
2023-09-19 23:10:45 +0500

    modal comments

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,80 @@
+import styles from "./comment.module.scss";
+import black from "../../assets/icons/black.jpeg";
+
+export const Comment = () => {
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <img src={black} alt="" className={styles.avatar} />
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.top}>
+              <p>Вячеслав</p>
+              <span>@slava</span>
+              <span>2 ч</span>
+            </div>
+            <p className={styles.comment}>Пошел на хер!</p>
+          </div>
+        </div>
+        <div className={styles.bottom_line}></div>
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <img src={black} alt="" className={styles.avatar} />
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.top}>
+              <p>Вячеслав</p>
+              <span>@slava</span>
+              <span>2 ч</span>
+            </div>
+            <p className={styles.comment}>Пошел на хер!</p>
+          </div>
+        </div>
+        <div className={styles.bottom_line}></div>
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <img src={black} alt="" className={styles.avatar} />
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.top}>
+              <p>Вячеслав</p>
+              <span>@slava</span>
+              <span>2 ч</span>
+            </div>
+            <p className={styles.comment}>Пошел на хер!</p>
+          </div>
+        </div>
+        <div className={styles.bottom_line}></div>
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <img src={black} alt="" className={styles.avatar} />
+          </div>
+
+          <div className={styles.right}>
+            <div className={styles.top}>
+              <p>Вячеслав</p>
+              <span>@slava</span>
+              <span>2 ч</span>
+            </div>
+            <p className={styles.comment}>Пошел на хер!</p>
+          </div>
+        </div>
+        <div className={styles.bottom_line}></div>
+      </div>
+    </>
+  );
+};
